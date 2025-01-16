10d97937fac1877dd94f6673fa242e41ab530117
rahul verma <rahulvermacode@gmail.com>
2025-01-16 11:25:53 +0530

    update

---
+++ week-2-asyn js/1index.js
@@ -0,0 +1,8 @@
+function sum(a,b){
+    return a+b;
+}
+
+let Sum=sum(4,5);
+console.log(Sum);
+
+
---
+++ week-2-asyn js/2index.js
@@ -0,0 +1,14 @@
+
+
+function sum(n){
+    let ans=0;
+    for(let i=1;i<=n;i++){
+        ans=ans+i;
+        }
+        return ans;
+
+}
+
+Sum=sum(3);
+
+console.log(Sum);
