e2d555b408fa7337ff47a9704dec6cc230bb9af5
rahul verma <rahulvermacode@gmail.com>
2025-01-16 10:56:38 +0530

    update

---
+++ week-1-bascic-of-js/5if-else.js
@@ -0,0 +1,7 @@
+let age=19;
+if(age>=18){
+    console.log("you can vote");
+}
+else{
+    console.log("you can't vote");
+}
---
+++ week-1-bascic-of-js/5loop.js
@@ -0,0 +1,9 @@
+for(let i=1;i<=10;i++){
+    console.log("this is my loop",i);
+}
+
+let j=0;
+while(j<=5){
+    console.log(j);
+    j++;
+}
